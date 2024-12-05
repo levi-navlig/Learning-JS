4ed69dc69b55876bc3ed670e195fb46682b96bd1
Levi-Navlig <levinavlig1227@gmail.com>
2024-12-05 17:23:22 -0300

    Alterações no Tópico 2

---
+++ Tópico 2/index.js
@@ -0,0 +1,35 @@
+// Definindo as variáveis
+let x = 100;
+let z = 50;
+
+// Operadores básicos
+
+console.log(x + z);
+console.log(x - z);
+console.log(x / z);
+console.log(x * z);
+
+// Operadores "complexos"
+
+console.log(x % z);
+console.log(x ++);
+console.log(z --);
+console.log(x += z);
+console.log(x -= z);
+console.log(x *= z);
+console.log(x /= z);
+console.log(x %= z);
+
+// Operadores de comparação
+
+console.log(x > z);
+console.log(x <= z);
+console.log(x == z);
+console.log(x != z);
+
+// Operadores Lógicos
+
+
+console.log(true && true);
+console.log(false || true);
+console.log(! true);
